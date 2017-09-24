c6e7c85f6730f9117194f36049d3d7adeb8e14d8
Vitalii Saienko <modestfake@gmail.com>
2017-09-24 14:56:43 +0300

    Remove hash

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import photo from '../assets/img/test.jpg'
-import hash from 'object-hash'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>{hash('Go ahead and create a cool app!')}</h1>
+        <h1>Go ahead and create a cool app!</h1>
         <img src={photo} />
       </div>
     )
